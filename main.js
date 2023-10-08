cc11068b3b6e24d6a2e0f03951cfa9abcfb507ee
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-10-08 10:24:26 -0300

    atualizacao de avatar

--- main.js
+++ main.js
@@ -11,6 +11,6 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     //se tiver  sem light mode manter a imagem normal
-    img.setAttribute("src", "./assets/Avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
